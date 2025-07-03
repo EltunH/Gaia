d4f20308e6a39a991b6e57f66a8bb3a2f1d38564
EltunH <eltun.huseynov17@gmail.com>
2025-07-03 11:16:33 +0400

    change move img

--- src/mycomponents/header/Moving.jsx
+++ src/mycomponents/header/Moving.jsx
@@ -18,14 +18,14 @@ export function Moving() {
 
     return (
         <div className="mt-12 overflow-x-hidden">
-            <div ref={boxRef} style={{ transform: `translateX(${scrollX}px)`, }} className="flex w-[120%] gap-4 md:gap-6 translate-x-[150px] md:translate-x-[50px]">
+            <div ref={boxRef} style={{ transform: `translateX(${scrollX}px)`, }} className="flex w-[120%] gap-4 md:gap-6 translate-x-[150px] mini:translate-x-[400px] md:translate-x-[150px] lg:translate-x-[50px]">
                 {imgArr1.map((item, index) => (
                     <div key={index} className="min-w-[150px] w-[25%]">
                         <img src={`/assets/img/${item}.jpg`} alt={`photo-${index}`} className="hover:scale-[.98] transition-all duration-300 cursor-pointer rounded-[8px] w-full h-full lg:h-[400px]" />
                     </div>
                 ))}
             </div>
-            <div ref={boxRef} style={{ transform: `translateX(${scrollX1}px)`, }} className="w-[120%] flex mt-4 md:mt-6 gap-4 md:gap-6 translate-x-[-400px] mini:translate-x-[-380px] md:translate-x-[-300px]">
+            <div ref={boxRef} style={{ transform: `translateX(${scrollX1}px)`, }} className="w-[120%] flex mt-4 md:mt-6 gap-4 md:gap-6 translate-x-[-500px] mini:translate-x-[-650px] md:translate-x-[-350px]">
                 {imgArr2.reverse().map((item, index) => (
                     <div key={index} className="min-w-[150px] w-[25%]">
                         <img src={`/assets/img/${item}.jpg`} alt={`photo-${index}`} className="hover:scale-[.98] transition-all duration-300 cursor-pointer rounded-[8px] w-full h-full lg:h-[400px]" />
