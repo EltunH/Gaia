37d97d521007ed80094f06878caa8136babf6cf1
EltunH <eltun.huseynov17@gmail.com>
2025-07-07 14:44:28 +0400

    Update button styles for improved accessibility and visual consistency

--- src/mycomponents/header/Established.jsx
+++ src/mycomponents/header/Established.jsx
@@ -84,9 +84,9 @@ export function Established() {
                     <p className="text-[#e0e1cc] text-center text-[16px] md:text-[18px] font-medium">Our goal then and now is to provide quality on time projects</p>
                 </div>
                 <div className="grid place-items-center mt-4 md:mt-6">
-                    <a href="tel:+994555555555" className="uppercase h-[57px] group gap-4 flex items-center cursor-pointer hover:scale-[.95] pl-4 pr-2 rounded-full text-[14px] font-[900] bg-[#3fb33d] text-currColor max-lmd:mr-4 transition-all duration-300">
+                    <a href="tel:+994555555555" className="uppercase h-[57px] group gap-4 flex items-center cursor-pointer hover:scale-[.95] pl-4 pr-2 rounded-full text-[14px] font-[900] bg-currColor text-white max-lmd:mr-4 transition-all duration-300">
                         <span className="text-nowrap">Send request</span>
-                        <div className="w-[40px] h-[40px] group-hover:bg-[#0f623a] text-white group-hover:rotate-[-45deg] group-hover:scale-[.95] rounded-full grid place-items-center bg-currColor transition-all duration-300">
+                        <div className="w-[40px] h-[40px] group-hover:bg-[#0f623a] text-white group-hover:rotate-[-45deg] group-hover:scale-[.95] rounded-full grid place-items-center bg-[#3fb33d] transition-all duration-300">
                             <FiArrowRight />
                         </div>
                     </a>
